418baa68292e0c0dc41d64dc6bf4fced87a82b96
Matthew Smith <matthewjsmith0523@gmail.com>
2018-10-21 17:09:02 -0500

    Update Our Products 10-18-16 a.py

---
+++ Our Products 10-18-16 a.py
@@ -0,0 +1,237 @@
+<h1 class="sbWidget" id="searchTitle" style="text-align: center;"><strong>Our Handcrafted Soaps</strong></h1>
+
+<div class="column1">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/oat_milk_honey.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Oatmeal, Milk, &amp; Honey</strong></h2>
+
+<div class="product-btn">
+<p><a href="/oatmeal-milk-honey.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column2">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/lavender-oatmeal.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Lavender Oatmeal</strong></h2>
+
+<div class="product-btn">
+<p><a href="/lavender-oatmeal.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column3">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/watermark.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Exotic Plumeria</strong></h2>
+
+<div class="product-btn">
+<p><a href="/exotic-plumeria.html">Read More</a></p>
+</div>
+</div>
+
+<div class="clear">&nbsp;</div>
+
+<div class="column1">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/harmony.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Harmony</strong></h2>
+
+<div class="product-btn">
+<p><a href="/harmony.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column2">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/watermark.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Pure Citrus</strong></h2>
+
+<div class="product-btn">
+<p><a href="/pure-citrus.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column3">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/watermark.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Orange Patchouli</strong></h2>
+
+<div class="product-btn">
+<p><a href="/orange-patchouli.html">Read More</a></p>
+</div>
+</div>
+
+<div class="clear">&nbsp;</div>
+
+<div class="column1">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/watermark.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Egyptian Musk</strong></h2>
+
+<div class="product-btn">
+<p><a href="/egyptian-musk.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column2">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/woodlands.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>The Woodlands</strong></h2>
+
+<div class="product-btn">
+<p><a href="/the-woodlands.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column3">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/eucalyptus-spearmint.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Spearmint &amp; Eucalyptus</strong></h2>
+
+<div class="product-btn">
+<p><a href="/spearmint--eucalyptus.html">Read More</a></p>
+</div>
+</div>
+
+<div class="clear">&nbsp;</div>
+
+<div class="column1">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/watermark.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Sea Salt &amp; Lotus Blossom</strong></h2>
+
+<div class="product-btn">
+<p><a href="/sea-salt-lotus-blossom.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column2">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/watermark.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Simply Amazing!</strong></h2>
+
+<div class="product-btn">
+<p><a href="/simply-amazing.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column3">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/watermark.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Mystical Lime</strong></h2>
+
+<div class="product-btn">
+<p><a href="/mystical-lime.html">Read More</a></p>
+</div>
+</div>
+
+<div class="clear">&nbsp;</div>
+
+<div class="column1">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/white-cotton.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>White Cotton</strong></h2>
+
+<div class="product-btn">
+<p><a href="/white-cotton.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column2">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/onthebeach.jpg" style="width: 250px; height: 161px;" /><br />
+<strong>Just Beachy</strong></h2>
+
+<div class="product-btn">
+<p><a href="/just-beachy.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column3">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/watermark.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Gentle Orange</strong></h2>
+
+<div class="product-btn">
+<p><a href="/gentle-orange.html">Read More</a></p>
+</div>
+</div>
+
+<div class="clear">&nbsp;</div>
+
+<div class="column1">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/ocean-mist.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Beach Breeze</strong></h2>
+
+<div class="product-btn">
+<p><a href="/beach-breeze.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column2">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/sea-salt.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Sea Salt and Sage</strong></h2>
+
+<div class="product-btn">
+<p><a href="/sea-salt-and-sage.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column3">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/orange-spearmint.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Orange Spearmint&nbsp;</strong></h2>
+
+<div class="product-btn">
+<p><a href="/orange-spearmint.html">Read More</a></p>
+</div>
+</div>
+
+<div class="clear">&nbsp;</div>
+
+<div class="column1">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/watermark.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Juicy Orange</strong></h2>
+
+<div class="product-btn">
+<p><a href="/juicy-orange.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column2">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/olive-oil.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Fabulous Olive</strong></h2>
+
+<div class="product-btn">
+<p><a href="/fabulous-olive.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column3">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/watermark.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Celebrate</strong></h2>
+
+<div class="product-btn">
+<p><a href="/celebrate.html">Read More</a></p>
+</div>
+</div>
+
+<div class="clear">&nbsp;</div>
+
+<div class="column1">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/watermark.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Unique</strong></h2>
+
+<div class="product-btn">
+<p><a href="/unique.html">Read More</a></p>
+</div>
+</div>
+
+<div class="column2">&nbsp;</div>
+
+<div class="column3">&nbsp;</div>
+
+<div class="clear">&nbsp;</div>
+
+<hr />
+<h1 class="sbWidget" id="searchTitle" style="text-align: center;"><strong>Soap Accessories</strong>​</h1>
+
+<div class="column1">
+<h2 style="text-align: center;"><img alt="" id="products-img" src="/images/soap/watermark.jpg" style="height: 161px; width: 250px;" /><br />
+<strong>Coming Soon</strong></h2>
+
+<div class="product-btn">
+<p><a href="#">Read More</a></p>
+</div>
+</div>
+
+<div class="column2">&nbsp;</div>
+
+<div class="column3">&nbsp;</div>
+
+<div class="clear">&nbsp;</div>
