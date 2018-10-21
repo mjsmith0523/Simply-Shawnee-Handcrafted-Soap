2e910a43962d375bb4d965efd0227ace1f129ff9
Matthew Smith <matthewjsmith0523@gmail.com>
2018-10-21 17:09:36 -0500

    Update Simply Shawnee Handcrafted Soap - Product html code.py

---
+++ Simply Shawnee Handcrafted Soap - Product html code.py
@@ -0,0 +1,77 @@
+
+<div class="column1">
+<div class="product-fold">
+<ul>
+	<li class="block"><input id="item1" name="item" type="checkbox" />
+	<p><label for="item1"><img alt="" src="/images/oatmeal-milk-honey.jpg" style="width: 250px; height: 375px;" /> Oatmeal, Milk, &amp; Honey</label></p>
+
+	<ul class="options">
+		<li style="list-style: none;">
+		<p><strong>Product&nbsp;Discription</strong><br />
+		Tebopun pade saseg niti obito; ca mege betasos. Pe enosaso tase he pemoh eramihe? Ya erepo te ru gepe ekimen, jerene roc torafa lu nilim hafoni colof. Ileg sin danem. Mesinib te apedel lel, epo igo oseri esekil onunenil ha otaseb gamasim mot lec. Wapi rin idel mede sedoke po casora.&nbsp;</p>
+		</li>
+		<li style="list-style: none;">
+		<p><strong>Price</strong><br />
+		$00 + Shipping&nbsp;</p>
+		</li>
+		<li style="list-style: none;">
+		<p><strong>Order Here</strong><br />
+		Button to Shop</p>
+		</li>
+	</ul>
+	</li>
+</ul>
+</div>
+</div>
+
+<div class="column2">
+<div class="product-fold">
+<ul>
+	<li class="block"><input id="item1" name="item" type="checkbox" />
+	<p><label for="item1"><img alt="" src="/images/wisteria-lilac.jpg" style="width: 250px; height: 375px;" /> Wisteria &amp; Lilac</label></p>
+
+	<ul class="options">
+		<li style="list-style: none;">
+		<p><strong>Product&nbsp;Discription</strong><br />
+		Tebopun pade saseg niti obito; ca mege betasos. Pe enosaso tase he pemoh eramihe? Ya erepo te ru gepe ekimen, jerene roc torafa lu nilim hafoni colof. Ileg sin danem. Mesinib te apedel lel, epo igo oseri esekil onunenil ha otaseb gamasim mot lec. Wapi rin idel mede sedoke po casora.&nbsp;</p>
+		</li>
+		<li style="list-style: none;">
+		<p><strong>Price</strong><br />
+		$00 + Shipping&nbsp;</p>
+		</li>
+		<li style="list-style: none;">
+		<p><strong>Order Here</strong><br />
+		Button to Shop</p>
+		</li>
+	</ul>
+	</li>
+</ul>
+</div>
+</div>
+
+<div class="column3">
+<div class="product-fold">
+<ul>
+	<li class="block"><input id="item1" name="item" type="checkbox" />
+	<p><label for="item1"><img alt="" src="/images/exotic-plumeria.jpg" style="width: 250px; height: 375px;" /> Exotic Plumeria</label></p>
+
+	<ul class="options">
+		<li style="list-style: none;">
+		<p><strong>Product&nbsp;Discription</strong><br />
+		Tebopun pade saseg niti obito; ca mege betasos. Pe enosaso tase he pemoh eramihe? Ya erepo te ru gepe ekimen, jerene roc torafa lu nilim hafoni colof. Ileg sin danem. Mesinib te apedel lel, epo igo oseri esekil onunenil ha otaseb gamasim mot lec. Wapi rin idel mede sedoke po casora.&nbsp;</p>
+		</li>
+		<li style="list-style: none;">
+		<p><strong>Price</strong><br />
+		$00 + Shipping&nbsp;</p>
+		</li>
+		<li style="list-style: none;">
+		<p><strong>Order Here</strong><br />
+		Button to Shop</p>
+		</li>
+	</ul>
+	</li>
+</ul>
+</div>
+</div>
+
+<div class="clear">&nbsp;</div>
